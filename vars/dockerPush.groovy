4f2814b92793a32c412c706368552615e181c001
Ayush Jasrotia(UST <299773@ust.com>
2025-10-22 15:01:47 +0530

    FIX secret text random

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,10 +1,7 @@
 def call(String project, String ImageTag, String hubUser){
     withCredentials([string(credentialsId: 'docker', variable: 'PASS')]) {
         sh "docker login -u ${hubUser} -p ${PASS}"
+        sh "docker push ${hubUser}/${project}:${ImageTag}"
+        sh "docker push ${hubUser}/${project}:latest"
     }
-
-    sh """
-        docker push ${hubUser}/${project}:${ImageTag}
-        docker push ${hubUser}/${project}:latest
-    """
 }
