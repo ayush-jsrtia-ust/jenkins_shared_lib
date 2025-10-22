771c96fa0e4035eab3b8969062189a9e4b12d033
Ayush Jasrotia(UST <299773@ust.com>
2025-10-22 14:33:00 +0530

    ADD docker push

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,10 @@
+def call(String project, String ImageTag, String hubUser){
+    withCredentials([string(credentialsId: 'docker', variable: 'PASS')]) {
+        sh "docker login -u ${hubUser} -p ${PASS}"
+    }
+
+    sh """
+        docker push ${hubUser}/${project}:${ImageTag}
+        docker push ${hubUser}/${project}:latest
+    """
+}
