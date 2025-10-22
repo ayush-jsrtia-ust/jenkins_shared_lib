09e09c4a2a948939ad26e1ee83cd11e34df10221
Ayush Jasrotia(UST <299773@ust.com>
2025-10-22 16:34:02 +0530

    ADD ECR groovys

---
+++ vars/dockerPushECR.groovy
@@ -0,0 +1,6 @@
+def call(String aws_account_id, String region, String ecr_repo_name){
+    sh """
+        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+        docker push ${ecr_repo_name}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest
+    """
+}
