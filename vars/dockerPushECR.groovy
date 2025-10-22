d17e89f684add13f2161c07a5ad2fb9bf2168d16
Ayush Jasrotia(UST <299773@ust.com>
2025-10-23 00:03:38 +0530

    FIX docker ecr login

--- vars/dockerPushECR.groovy
+++ vars/dockerPushECR.groovy
@@ -1,8 +1,8 @@
 def call(String aws_account_id, String region, String ecr_repo_name){
     sh """
-        export ECR_PASSWORD=$(aws ecr get-login-password --region ${region})
-        docker login --username AWS --password $ECR_PASSWORD ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+        export ECR_PASSWORD=\$(aws ecr get-login-password --region ${region})
+        docker login --username AWS --password \$ECR_PASSWORD ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
         docker tag ${ecr_repo_name}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest
         docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest
     """
-}
+}   
